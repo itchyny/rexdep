ce8e4cc9632b06903834b4733b94d8d1b7dc928e
itchyny <itchyny@hatena.ne.jp>
2015-12-07 08:24:28 +0900

    update main.go: version bump up to v0.0.4

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "os"
 
 var name = "rexdep"
-var version = "v0.0.3"
+var version = "v0.0.4"
 var description = "Roughly extract dependency from source code"
 var author = "itchyny"
 
