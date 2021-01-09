db34c66add38c91c3ab8ba0e42d76ac43c999f4e
itchyny <itchyny@hatena.ne.jp>
2021-01-10 01:12:37 +0900

    bump up version to 0.3.1

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "os"
 
 var name = "rexdep"
-var version = "0.3.0"
+var version = "0.3.1"
 var description = "Roughly extract dependency from source code"
 var author = "itchyny"
 
